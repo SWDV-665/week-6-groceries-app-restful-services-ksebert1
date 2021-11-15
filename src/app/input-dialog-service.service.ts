6d9bcb7d278277c6b590262c0426506ffe11c78f
ksebert1 <ksebert1@live.maryville.edu>
2021-11-14 20:46:26 -0600

    Add style to text for empty list

--- src/app/input-dialog-service.service.ts
+++ src/app/input-dialog-service.service.ts
@@ -1,9 +1,205 @@
 import { Injectable } from '@angular/core';
+import { GroceriesServiceService } from './groceries-service.service';
+
+
+
+// @Component ({
+// selector: 'select-example',
+// templateJitUrl: 'select-example.html',
+// styleUrls: ['./select-example.css']
+// })
+
+
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class InputDialogServiceService {
 
-  constructor() { }
+  constructor(public dataService: GroceriesServiceService) { }
+
+  // presentItemPrompt() {
+  //   const presentAlert = document.createElement('ion-alert');
+  //   presentAlert.cssClass = 'my-custom-class';
+  //   presentAlert.header = 'Enter Item';
+  //   presentAlert.inputs = [
+  //      {
+  //       name: 'brand',
+  //       id: 'itemBrand',
+  //       placeholder: 'Enter item brand information.'
+  //     },
+  //     {
+  //       name: 'name',
+  //       id: 'itemName',
+  //       placeholder: 'Enter item name.'
+  //     },
+  //     {
+  //       name: 'size',
+  //       id: 'itemSize',
+  //       placeholder: 'Enter item size (14 oz., loaf, box, etc.)'
+  //     },
+
+  //     {
+  //       name: 'quantity',
+  //       placeholder: 'Enter amount to add',
+  //       type: 'number',
+  //       min: 1,
+  //       max: 999999999999
+  //     }
+  
+  //   ];
+  //   presentAlert.buttons = [
+  //     {
+  //       text: 'Cancel',
+  //       role: 'cancel',
+  //       cssClass: 'secondary',
+  //       handler: () => {
+  //         console.log('Confirm Cancel')
+  //       }
+  //     }, {
+  //       text: 'Ok',
+  //       handler: item => {
+  //         console.log('Confirm Ok')
+  //         this.dataService.addItem(item);
+  //       }
+  //     }
+  //   ]
+  
+  //   document.body.appendChild(presentAlert);
+  //   return presentAlert.present();
+  // }
+  
+  //   editItemPrompt(item,i) {
+  //       const editAlert = document.createElement('ion-alert');
+  //       editAlert.cssClass = 'my-custom-class';
+  //       editAlert.header = 'Enter Item';
+  //       editAlert.inputs = [
+  //          {
+  //           name: 'brand',
+  //           id: 'itemBrand',
+  //           value: item.brand
+           
+  //         },
+  //         {
+  //           name: 'name',
+  //           id: 'itemName',
+  //           value: item.name
+  //         },
+  //         {
+  //           name: 'size',
+  //           id: 'itemSize',
+  //           value: item.size
+  //         },
+
+  //         {
+  //           name: 'quantity',
+  //           placeholder: 'Enter amount to add',
+  //           type: 'number',
+  //           min: 1,
+  //           max: 999999999999,
+  //           value: item.quantity
+  //         }
+      
+  //       ]
+  //       editAlert.buttons = [
+  //         {
+  //           text: 'Cancel',
+  //           role: 'cancel',
+  //           cssClass: 'secondary',
+  //           handler: () => {
+  //             console.log('Confirm Cancel')
+  //           }
+  //         }, {
+  //           text: 'Ok',
+  //           handler: item => {
+  //             console.log('Confirm Ok')
+  //             this.dataService.editItem(item, i);
+  //           }
+  //         }
+  //       ]
+  
+  //       document.body.appendChild(editAlert);
+  //       return editAlert.present();
+
+  //     }
+
+        showPrompt(item?,i?) {
+          const prompt = document.createElement('ion-alert');
+          prompt.cssClass = 'my-custom-class';
+          prompt.header = item ? 'Edit Item' : "Add Item";
+          prompt.message = item ? "Please Edit Item..." : "Please Add Item..."
+          prompt.inputs = [
+             {
+              name: 'brand',
+              placeholder: 'Brand',
+              id: 'itemBrand',
+              value: item ? item.brand : null
+             
+            },
+            {
+              name: 'name',
+              placeholder: 'Name of Item',
+              id: 'itemName',
+              value: item ? item.name : null
+            },
+            {
+              name: 'size',
+              placeholder: 'Size, e.g. loaf or 14. oz, box',
+              id: 'itemSize',
+              value: item ? item.size : null
+            },
+            {
+              name: 'quantity',
+              placeholder: 'Enter amount of item',
+              type: 'number',
+              min: 1,
+              max: 999999999999,
+              value: item ?  item.quantity : null
+            }
+        
+          ]
+
+          prompt.buttons = [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              cssClass: 'secondary',
+              handler: () => {
+                console.log('Confirm Cancel')
+              }
+            }, {
+              text: 'Ok',
+              handler: item => {
+                console.log('Confirm Ok', item)
+                if (i !== undefined) {
+                  this.dataService.editItem(item, i);
+                }
+                else {
+                  this.dataService.addItem(item);
+                }
+
+              }
+            }
+          ]
+    
+          document.body.appendChild(prompt);
+          return prompt.present();
+
+
+
+
+
+      }
+  
+          
+  
+  
+  
+  
+    
+  
+  
+
+
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { GroceriesServiceService } from '../groceries-service.service';
+import { InputDialogServiceService } from '../input-dialog-service.service';
 
 
 @Component({
@@ -13,18 +14,18 @@ export class Tab1Page {
 
  title = "Grocery List Items";
 
-constructor(public dataService: GroceriesServiceService) {}
+constructor(public dataService: GroceriesServiceService, public InputDialogServiceService: InputDialogServiceService) {}
 
 loadItems(){
   return this.dataService.getItems();
 }
 
 addItem(){
-  this.presentItemPrompt()
+  this.InputDialogServiceService.showPrompt()
 }
 
 editItem(item,i){
-this.editItemPrompt(item,i)
+this.InputDialogServiceService.showPrompt(item,i)
 }
 
 
@@ -32,115 +33,6 @@ removeItem(item,i){
   console.log("Removing Item - .", item,i)
   this.dataService.removeItem(i);
 } 
-
-presentItemPrompt() {
-  const presentAlert = document.createElement('ion-alert');
-  presentAlert.cssClass = 'my-custom-class';
-  presentAlert.header = 'Enter Item';
-  presentAlert.inputs = [
-     {
-      name: 'brand',
-      id: 'itemBrand',
-      placeholder: 'Enter item brand information.'
-    },
-    {
-      name: 'size',
-      id: 'itemSize',
-      placeholder: 'Enter item size (14 oz., loaf, box, etc.)'
-    },
-    {
-      name: 'name',
-      id: 'itemName',
-      placeholder: 'Enter item name.'
-    },
-    {
-      name: 'quantity',
-      placeholder: 'Enter amount to add',
-      type: 'number',
-      min: 1,
-      max: 999999999999
-    }
-
-  ];
-  presentAlert.buttons = [
-    {
-      text: 'Cancel',
-      role: 'cancel',
-      cssClass: 'secondary',
-      handler: () => {
-        console.log('Confirm Cancel')
-      }
-    }, {
-      text: 'Ok',
-      handler: item => {
-        console.log('Confirm Ok')
-        this.dataService.addItem(item);
-      }
-    }
-  ]
-
-  document.body.appendChild(presentAlert);
-  return presentAlert.present();
-}
-
-  editItemPrompt(item,i) {
-      const editAlert = document.createElement('ion-alert');
-      editAlert.cssClass = 'my-custom-class';
-      editAlert.header = 'Enter Item';
-      editAlert.inputs = [
-         {
-          name: 'brand',
-          id: 'itemBrand',
-          value: item.brand
-         
-        },
-        {
-          name: 'size',
-          id: 'itemSize',
-          value: item.size
-        },
-        {
-          name: 'name',
-          id: 'itemName',
-          value: item.name
-        },
-        {
-          name: 'quantity',
-          placeholder: 'Enter amount to add',
-          type: 'number',
-          min: 1,
-          max: 999999999999,
-          value: item.quantity
-        }
-    
-      ]
-      editAlert.buttons = [
-        {
-          text: 'Cancel',
-          role: 'cancel',
-          cssClass: 'secondary',
-          handler: () => {
-            console.log('Confirm Cancel')
-          }
-        }, {
-          text: 'Ok',
-          handler: item => {
-            console.log('Confirm Ok')
-            this.dataService.editItem(item, i);
-          }
-        }
-      ]
-
-      document.body.appendChild(editAlert);
-      return editAlert.present();
-    }
-
-        
-
-
-
-
-  
 
 
 
